f76b0a9bbef42ef1572c1a3d0f18f9917f53b244
Diego Cordoba <diegocordoba87@hotmail.com>
2020-09-10 09:54:18 -0400

    added navbar

---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NavBar = () => {
+    return (
+        <nav class="nav flex-column">
+  <a class="nav-link active" href="#">judas</a>
+  <a class="nav-link" href="#">Pictures</a>
+  <a class="nav-link" href="#">Video</a>
+        </nav>
+    );
+};
+
+export default NavBar;
