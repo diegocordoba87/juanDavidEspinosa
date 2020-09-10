a23342e28c44e00cb993834dcc148f76fc1a141f
Diego Cordoba <diegocordoba87@hotmail.com>
2020-09-10 12:27:36 -0400

    changed navBar width

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -2,11 +2,16 @@ import React from 'react';
 
 const NavBar = () => {
     return (
-        <nav class="nav flex-column">
-  <a class="nav-link active" href="#">judas</a>
-  <a class="nav-link" href="#">Pictures</a>
-  <a class="nav-link" href="#">Video</a>
+        <div className="col-1">
+<nav className="nav flex-column">
+  <a style={{marginTop: 80, fontSize:30, fontWeight:10}}className="nav-link active" href="#">JUDAS</a>
+  <a className="nav-link" href="#">Pictures</a>
+  <a className="nav-link" href="#">Video</a>
+  <a className="nav-link" href="#">About</a>
+  <a className="nav-link" href="#">Contact</a>
         </nav>
+        </div>
+ 
     );
 };
 
