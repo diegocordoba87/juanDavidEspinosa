b761f384291aacf175710ad707a1280a81495a44
Diego Cordoba <diegocordoba87@hotmail.com>
2020-09-23 10:40:29 -0400

    changed home page added image

--- client/src/containers/Home.jsx
+++ client/src/containers/Home.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import dayDreaming from "../../src/media/gifs/judas_daydreaming.gif"
-import Photography from "../../src/media/gifs/judas_photography.gif"
+import Photography from "../../src/media/gifs/judas_photography.png"
 
 class Home extends Component {
     render() {
@@ -12,11 +12,11 @@ class Home extends Component {
             <div>
                 <div className="container">
                     <div className="row">
-                    <div className= "col">
-                        <img style={{marginTop:20}}src={dayDreaming} alt="dayDreaming" />
+                    <div className= "col-lg-6 col-sm-12">
+                        <img style={{marginTop:20, width:'100%'}}src={dayDreaming} alt="dayDreaming" />
                     </div>
-                    <div className= "col">
-                        <img style={{marginTop:20}}src={Photography} alt="dayDreaming" />
+                    <div className= "col-lg-6 col-sm-12">
+                        <img style={{marginTop:20, marginLeft: 10, width: '100%'}}src={Photography} alt="dayDreaming" />
                     </div>
                     </div>
                 </div>
