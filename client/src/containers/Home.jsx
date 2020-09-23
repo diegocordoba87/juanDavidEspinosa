45fde3fff5796767a26770fabf9772d5ae03a300
Diego Cordoba <diegocordoba87@hotmail.com>
2020-09-23 11:06:00 -0400

    added hover effect to gif

--- client/src/containers/Home.jsx
+++ client/src/containers/Home.jsx
@@ -1,30 +1,45 @@
-import React, {Component} from 'react';
-import dayDreaming from "../../src/media/gifs/judas_daydreaming.gif"
-import Photography from "../../src/media/gifs/judas_photography.png"
+import React, { Component } from "react";
+import "../style/style.css";
+import dayDreaming from "../../src/media/gifs/judas_daydreaming.gif";
+import Photography from "../../src/media/gifs/judas_photography.png";
 
 class Home extends Component {
-    render() {
-
-        const iconSize ={
-            fontSize: 30
-        }
-        return (
-            <div>
-                <div className="container">
-                    <div className="row">
-                    <div className= "col-lg-6 col-sm-12">
-                        <img style={{marginTop:20, width:'100%'}}src={dayDreaming} alt="dayDreaming" />
-                    </div>
-                    <div className= "col-lg-6 col-sm-12">
-                        <img style={{marginTop:20, marginLeft: 10, width: '100%'}}src={Photography} alt="dayDreaming" />
-                    </div>
-                    </div>
+  render() {
+    const iconSize = {
+      fontSize: 30,
+    };
+    return (
+      <div>
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-6 col-sm-12">
+              <div className="hovereffect">
+                <img
+                  className="img-responsive"
+                  style={{ marginTop: 20, width: "100%" }}
+                  src={dayDreaming}
+                  alt="dayDreaming"
+                />
+                <div className="overlay">
+                  <h2>Videos</h2>
+                  <p>
+                    <a href="#">Check out my videos</a>
+                  </p>
                 </div>
+              </div>
             </div>
-                
-   
-        );
-    }
+            <div className="col-lg-6 col-sm-12">
+              <img
+                style={{ marginTop: 20, marginLeft: 10, width: "100%" }}
+                src={Photography}
+                alt="dayDreaming"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Home;
